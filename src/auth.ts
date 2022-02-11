019c2dce0ffd5af7837189d25e5befb2bd38a81c
Mendel <mendeljacks@gmail.com>
2022-02-11 00:39:03 -0500

    initial functions from old code

---
+++ src/auth.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import * as memoizee from 'memoizee'
+
+export type Auth = { user_name: string; password: string; subdomain: string }
+const safety_margin = 10000
+export const auth_to_cookie = memoizee(
+    async (auth: Auth) => {
+        const login_response = await axios.post(
+            `https://${auth.subdomain}.goflowapp.com/Account/PostLogin`,
+            {
+                userName: auth.user_name,
+                password: auth.password,
+                subdomain: auth.subdomain
+            }
+        )
+
+        const headers = login_response.headers || {}
+        const set_cookie = headers['set-cookie'] || []
+        const cookie_head = set_cookie[0] || ''
+        const cookie_head_split = cookie_head.split(';')
+        const cookie_head_split_0 = cookie_head_split[0] || ''
+        const cookie = cookie_head_split_0
+        return cookie
+    },
+    { promise: true, maxAge: 31536000 - safety_margin, primitive: true }
+)
+
+export const auth_to_base_url = (auth: Auth) => {
+    return `https://${auth.subdomain}.goflowapp.com`
+}
+
+export const login = async (auth: Auth) => {
+    return auth_to_cookie(auth)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export const goflow = () => {
-    console.log('hi from goflow')
+export const get_orders = (base_url: string, cookie: string, body: any) => {
+    return {
+        method: 'POST',
+        url: `${base_url}/api/order/list`,
+        data: body,
+        headers: { cookie }
+    }
+}
+
+export const order_get_order_id = async (
+    base_url: string,
+    cookie: string,
+    params: { orderId: number }
+) => {
+    return {
+        method: 'GET',
+        url: `${base_url}/api/order/get`,
+        params: params,
+        headers: { cookie }
+    }
 }
